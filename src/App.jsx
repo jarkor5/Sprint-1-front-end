1dcb0a503795763561c100fbbd8eaa6f3bb2f8dd
Jacobo <jacoor626@gmail.com>
2023-02-23 16:21:40 -0500

    se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div>
+      <header>
+        <nav>
+            <img src="./Menu.png" alt="Menu" class="Menu"> </img>
+            <img src="./Logo.svg" alt="Logo" class="logo"> </img>
+        </nav>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <nav>
+            <img src="./Menu.png" alt="Menu" class="Menu"> </img>
+            <img src="./Logo.svg" alt="Logo" class="logo"> </img>
+        </nav>
+  )
+}
